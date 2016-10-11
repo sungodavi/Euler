4c938c6d82ea99a3e66a6de4b7ccbf6b12870a5d
sungodavi <sungodavi@gmail.com>
2016-10-10 21:52:37 -0500

    Solved Problem 82

---
+++ Euler/src/Euler207.java
@@ -0,0 +1,26 @@
+
+public class Euler207 
+{
+	public static long solve(int numerator, int denominator)
+	{
+		long n = 1;
+		long d = 1;
+		long i;
+		for(i = 3; n * denominator >= d * numerator; i++)
+		{
+			long num = i * (i-1);
+			d++;
+			if((i & (i-1)) == 0)
+				n++;
+		}
+		return (i-1) * (i-2);
+	}
+	public static void main(String[] args)
+	{
+		long startTime = System.currentTimeMillis();
+		System.out.println(solve(1,12345));
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		System.out.println("Total time: " + (1.0 * totalTime / 1000) + " seconds");
+	}
+}
